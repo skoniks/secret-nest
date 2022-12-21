322c3ffa019d35d32fc604097bcf166b5818c50e
skoniks <skolya16@ya.ru>
2022-12-21 06:50:53 +0300

    DTO & Cors update

--- src/dto/open.dto.ts
+++ src/dto/open.dto.ts
@@ -1,7 +1,8 @@
-import { IsOptional, IsString } from 'class-validator';
+import { IsOptional, IsString, Length } from 'class-validator';
 
 export class OpenDTO {
   @IsString()
   @IsOptional()
+  @Length(1, 255)
   passphrase?: string;
 }
