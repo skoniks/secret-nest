2eb1f08960584af24cad3916e8023c7e1e1cee40
skoniks <skolya16@ya.ru>
2023-06-14 16:20:44 +0300

    Request entity limit update

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { json } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: { origin: '*' },
   });
+  app.use(json({ limit: '1mb' }));
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
   await app.listen(port);
